686040ac711a78fc11d6653dace3a7ad2c987855
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2019-01-20 11:51:45 +0530

    Before Status Code implementation

---
+++ WebApplication1/WebApplication1/CitiesDataStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore() ;
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    id = 1,
+                    Name = "Chennai City",
+                    Description = "With Beach"
+                },
+                new CityDto()
+                {
+                    id = 2,
+                    Name = "Vijayawada",
+                    Description = "With Threaters"
+                }
+            };
+        }
+    }
+}
